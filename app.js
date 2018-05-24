fea9d89956eaf290074829e93ff852a70248365b
Efrain Diaz <eudp_95@hotmail.com>
2018-05-24 11:16:49 -0400

    update code for promises

--- app.js
+++ app.js
@@ -33,91 +33,94 @@ function countLimit() {
 function sendMessage (paramsDm) {
 
 	T.post('direct_messages/new', paramsDm)
-		.then(function (data) {
-			console.log(`The message was sent with success to : @${paramsDm.screen_name}`);
-			countLimit();
-		}, function (err) {
-			console.log(`@${paramsDm.screen_name} | ${err[0].code} - ${err[0].message}`);
-			countLimit();
-		});
+	.then(function (data) {
+		return `The message was sent with success to : @${paramsDm.screen_name}`;
+	})
+	.catch(function (err) {
+		return `@${paramsDm.screen_name} | ${err[0].code} - ${err[0].message}`;
+	})
+	.then(function(data) {
+		console.log(data);
+		countLimit();
+	});
 }
 
 function sendCustomMessage (text) {
 
 	T.get('friends/list', paramsFriends)
-		.then(function(data) {
-			for (let i = 0; i < data.users.length; i++){
+	.then(function(data) {
+		for (let i = 0; i < data.users.length; i++){
 
-				let paramsDm = {
-					screen_name: data.users[i].screen_name,
-					text: text
-				}
-				sendMessage(paramsDm);
+			let paramsDm = {
+				screen_name: data.users[i].screen_name,
+				text: text
 			}
-		})
-		.catch(errHandlerClose);
+			sendMessage(paramsDm);
+		}
+	})
+	.catch(errHandlerClose);
 }
 
 function sendTrends () {
 
 	T.get('friends/list', paramsFriends)
-		.then( function (dataFriends) {
-			for (let i = 0; i < dataFriends.users.length; i++){
+	.then( function (dataFriends) {
+		for (let i = 0; i < dataFriends.users.length; i++){
 
-				new Promise (function(resolve, reject) {
+			new Promise (function(resolve, reject) {
 
-					request('http://woeid.rosselliot.co.nz/lookup/' + encodeURI(dataFriends.users[i].location.replace(',', '')),
+				request('http://woeid.rosselliot.co.nz/lookup/' + encodeURI(dataFriends.users[i].location.replace(',', '')),
 
-					    function (error, response, body) {
+				    function (error, response, body) {
 
-					    	if (error) {
-					    		reject(error);
-					    	} else {
-					    		let pos = body.search('data-woeid');
+				    	if (error) {
+				    		reject(error);
+				    	} else {
+				    		let pos = body.search('data-woeid');
 
-					        	if (pos !== -1) {
+				        	if (pos !== -1) {
 
-					        		let str = body.slice(pos+12, body.length);
-					      
-					        		let posEnd = str.indexOf('"');
+				        		let str = body.slice(pos+12, body.length);
+				      
+				        		let posEnd = str.indexOf('"');
 
-					        		let paramsTrends = {
-										id: str.slice(0,posEnd)
-									}
-									resolve(paramsTrends);
-					        	} else {
-					        		reject(`Couldn't find woeid for location of @${dataFriends.users[i].screen_name}`)
-					        	}
-					    	}
-					    }
-					);
-				})
-				.then(function(paramsTrends){
+				        		let paramsTrends = {
+									id: str.slice(0,posEnd)
+								}
+								resolve(paramsTrends);
+				        	} else {
+				        		reject(`Couldn't find woeid for location of @${dataFriends.users[i].screen_name}`)
+				        	}
+				    	}
+				    }
+				);
+			})
+			.then(function(paramsTrends){
 
-					return T.get('trends/place', paramsTrends);
-				})
-				.then(function (dataTrends) {
+				return T.get('trends/place', paramsTrends);
+			})
+			.then(function (dataTrends) {
 
-					let trendsStr = 'Trends based in your location: \n';
+				let trendsStr = 'Trends based in your location: \n';
 
-					let j = 0;
-					while (j < 5 && dataTrends[0].trends[j] !== undefined) {
-						trendsStr += `${dataTrends[0].trends[j].name}\n`;
-						j++;
-					}
+				let j = 0;
+				while (j < 5 && dataTrends[0].trends[j] !== undefined) {
+					trendsStr += `${dataTrends[0].trends[j].name}\n`;
+					j++;
+				}
 
-					let paramsDm = {
-						screen_name: dataFriends.users[i].screen_name,
-						text: trendsStr
-					}
+				let paramsDm = {
+					screen_name: dataFriends.users[i].screen_name,
+					text: trendsStr
+				}
 
-					sendMessage(paramsDm);
-				})
-				.catch(errHandlerCount);
-			}
+				sendMessage(paramsDm);
+			})
+			.catch(errHandlerCount);
+		}
 
-		})
-		.catch(errHandlerClose);
+	})
+	.catch(errHandlerClose);
 }
 
 function sendTweet (q) {
@@ -128,26 +131,27 @@ function sendTweet (q) {
 		result_type: 'popular'
 	}
 	let dataTweet;
+	
 	T.get('search/tweets', paramsTweet)
-		.then(function (data) { 
+	.then(function (data) { 
 
-			if (data.statuses.length === 0){
-				return Promise.reject(`There are not trends for the keyword`);
+		if (data.statuses.length === 0){
+			return Promise.reject(`There are not trends for the keyword`);
+		}
+		dataTweet = data; 
+		return T.get('friends/list', paramsFriends);
+	})
+	.then(function (dataUsers) {
+		for (let i = 0; i < dataUsers.users.length; i++){
+			let paramsDm = {
+				screen_name: dataUsers.users[i].screen_name,
+				text: `https://twitter.com/${dataTweet.statuses[0].user.screen_name}/status/${dataTweet.statuses[0].id_str}`
 			}
-			dataTweet = data; 
-			return T.get('friends/list', paramsFriends);
-		})
-		.then(function (dataUsers) {
-			for (let i = 0; i < dataUsers.users.length; i++){
-				let paramsDm = {
-					screen_name: dataUsers.users[i].screen_name,
-					text: `https://twitter.com/${dataTweet.statuses[0].user.screen_name}/status/${dataTweet.statuses[0].id_str}`
-				}
 
-				sendMessage(paramsDm);
-			}
-		})
-		.catch(errHandlerClose);
+			sendMessage(paramsDm);
+		}
+	})
+	.catch(errHandlerClose);
 }
 
 exports.sendCustomMessage = sendCustomMessage;
